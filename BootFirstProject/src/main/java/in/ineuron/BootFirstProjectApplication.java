b11032feae09c97a50c94e55ffb4b58fd22dca7d
Vikash Katiyar <rishabktr018@gmail.com>
2023-06-01 15:14:07 +0530

    First  Commit

---
+++ BootFirstProject/src/main/java/in/ineuron/BootFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootFirstProjectApplication.class, args);
+	}
+
+}
